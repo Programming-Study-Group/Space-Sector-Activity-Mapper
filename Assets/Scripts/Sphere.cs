976ee26ae23fe7e81818f2133f87f2a3b17ed7de
CahidArda <cahidardaooz@hotmail.com>
2020-10-03 21:21:58 +0300

    Made spheres selectable

---
+++ Assets/Scripts/Sphere.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public Material normalMaterial;
+    public Material selectedMaterial;
+    private static InfoManager infoManager;
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        if (infoManager==null)
+        {
+            infoManager = GameObject.FindObjectOfType<InfoManager>();
+        }
+    }
+
+    public void setColorToNormal()
+    {
+        renderer.material = normalMaterial;
+    }
+
+    public void setColorToSelected()
+    {
+        renderer.material = selectedMaterial;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked on " + transform.position);
+        infoManager.setSelectedSphere(this);
+    }
+}
