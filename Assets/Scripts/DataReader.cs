a074c4af7424526ab31913dbe1373616d27bc81f
CahidArda <cahidardaooz@hotmail.com>
2020-10-04 12:31:54 +0300

    Merged csv files

---
+++ Assets/Scripts/DataReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataReader : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+
+    //List<string[]> readData(string fileAddress)
+    void readData(string fileAddress)
+    {
+        try
+        {
+            // Create an instance of StreamReader to read from a file.
+            // The using statement also closes the StreamReader.
+            using (StreamReader sr = new StreamReader(fileAddress))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Debug.Log(line);
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            // Let the user know what went wrong.
+            Debug.Log(e.Message);
+            Debug.Log("There is no such file.");
+        }
+    }
+	
+}
